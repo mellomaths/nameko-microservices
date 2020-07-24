4937bf84860654d2311edd4bffa69edf27a1d240
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-07-24 09:34:41 -0300

    Add CartOut base model

--- gateway/app/api/carts/models.py
+++ gateway/app/api/carts/models.py
@@ -1,6 +1,20 @@
+from typing import List
+
 from pydantic import BaseModel
 
 
 class ProductIn(BaseModel):
     product_id: str
     amount: int
+
+
+class ProductOut(BaseModel):
+    id: str
+    amount: int
+    price: float
+
+
+class CartOut(BaseModel):
+    id: str
+    total_price: float
+    products: List[ProductOut]
